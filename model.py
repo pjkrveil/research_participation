937a464b134757e9f8602cf7ded78a5de4b2dbec
최재혁 <jake@Jakes-Macbook-Pro.local>
2017-11-30 02:57:10 +0900

    modify model.py

--- model.py
+++ model.py
@@ -1,8 +1,93 @@
+from keras import applications, optimizers
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Dropout
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
-from keras.optimizers import SGD
-import json
-import cv2, numpy as np
+from keras.layers import Dropout, Flatten, Dense, Input
+import keras
+
+print("++ keras version is: v.")
+print(keras.__version__)
+# OUTPUT: '2.0.8'
+
+keras.backend.set_image_dim_ordering('tf')
 
 
+# SET ALL THE PARAMETERS
+weights_path = 'models/vgg16.h5'
+top_model_weights_path = 'bottleneck_fc_model.h5'
+img_width, img_height = 224, 224
+train_data_dir = '/data/pjkrveil/train_val_images'
+validation_data_dir = '/data/pjkrveil/train_val_images'
+nb_train_samples = 500000
+nb_validation_samples = 79184
+epochs = 50
+batch_size = 50000
+
+
+
+# LOAD VGG16
+input_tensor = Input(shape=(224, 224, 3))
+model = applications.VGG16(weights='imagenet',
+                           include_top=False,
+                           input_tensor=input_tensor,
+                           classes=5089)
+
+
+# CREATE A TOP MODEL
+top_model = Sequential()
+top_model.add(Flatten(input_shape=model.output_shape[1:]))
+top_model.add(Dense(4096, activation='relu'))
+top_model.add(Dropout(0.5))
+top_model.add(Dense(1, activation='softmax'))
+top_model.load_weights(top_model_weights_path)
+
+
+# CREATE AN "REAL" MODEL FROM VGG16
+# BY COPYING ALL THE LAYERS OF VGG16
+new_model = Sequential()
+for l in model.layers:
+    new_model.add(l)
+
+
+# CONCATENATE THE TWO MODELS
+new_model.add(top_model)
+
+# LOCK THE TOP CONV LAYERS
+for layer in new_model.layers:
+    layer.trainable = False
+
+# COMPILE THE MODEL
+new_model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+              metrics=['accuracy'])
+
+
+
+# CREATE THE IMAGE GENERATORS
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+                        train_data_dir,
+                        target_size=(img_height,img_width),
+                        batch_size=batch_size,
+                        class_mode='binary')
+
+validation_generator = validation_datagen.flow_from_directory(
+                            validation_data_dir,
+                            target_size=(img_height,img_width),
+                            batch_size=batch_size,
+                            class_mode='binary')
+
+
+#  FIT THE MODEL
+
+new_model.fit_generator(
+    train_generator,
+    samples_per_epoch=nb_train_samples,
+    epochs=epochs,
+    validation_data=validation_generator,
+    nb_val_samples=nb_validation_samples)
