cf829e9394a2c0f30e890d8b7e7c77b2f9a15541
pjkrveil <pjkrveil@gmail.com>
2017-11-30 02:28:44 +0900

    add model.py

---
+++ model.py
@@ -0,0 +1,8 @@
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import json
+import cv2, numpy as np
+
+
